348e2a9b99a812e69d4ad4a6180e8d5963ad4f6a
Jiewen Huang <jhuang60@stuy.edu>
2026-05-20 14:35:28 -0400

    imported HexFormat which allows me to handle byte array to hex string conversion; previously, I had planned on using a function but this is more convenient

--- testing/TestSHA256.java
+++ testing/TestSHA256.java
@@ -3,6 +3,7 @@ import sha.SHA256;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.SecureRandom;
+import java.util.HexFormat;
 public class TestSHA256 {
     private static int passed = 0;
     private static int failed = 0;
@@ -28,10 +29,10 @@ public class TestSHA256 {
 
     private static void runTest(String input) {
         byte[] inputBytes = input.getBytes(StandardCharsets.UTF_8);
-        String ours = toHex(SHA256.hash(inputBytes));
+        String ours = hexFormat.of().formatHex(SHA256.hash(inputBytes));
         String expected;
         try {
-            expected = toHex(MessageDigest.getInstance("SHA-256").digest(inputBytes));
+            expected = hexFormat.of().formatHex(MessageDigest.getInstance("SHA-256").digest(inputBytes));
         } catch (Exception e) {
             System.out.println("ERROR: " + e.getMessage());
             failed++;
