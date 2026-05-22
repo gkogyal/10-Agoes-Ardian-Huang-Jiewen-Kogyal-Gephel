9da39a2586598c996331cd2f95bda755fd3db4b1
Ardian Agoes <ardianagoes83@gmail.com>
2026-05-22 19:56:22 -0400

    wrote method to add padding so length is a multiple of 16bytes

--- src/aes/Padding.java
+++ src/aes/Padding.java
@@ -2,5 +2,16 @@ package aes;
 
 public class Padding {
 
-  
+    /*
+    This method pads the input data with zeros to ensure that its length is a multiple of 16 bytes.
+    */
+    public static byte[] pad(byte[] data) {
+        int paddingLength = 16 - (data.length % 16);
+        byte[] paddedData = new byte[data.length + paddingLength];
+        System.arraycopy(data, 0, paddedData, 0, data.length);
+        for (int i = data.length; i < paddedData.length; i++) {
+            paddedData[i] = 0;
+        }
+        return paddedData;
+    }
 }
